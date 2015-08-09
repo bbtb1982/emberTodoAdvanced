41c0015c3aec7856592d9c4a51ef88d8569baecc
Brandon Richards <brandon@polevaultmedia.com>
2015-08-08 21:04:15 -0600

    about modal using render outlet. created applications route and modified template. added the about template and modal.

--- app/models/list.js
+++ app/models/list.js
@@ -1,16 +1,16 @@
 import Ember from 'ember';
 import DS from 'ember-data';
-export default DS.Model.extend({
+export default DS.Model.extend({    
 	title: DS.attr('string'),
-	isComplete: Ember.computed('todos', function(){
-		return this.get('todos').filterBy('isComplete', false).get('length') === 0;
-	}).property('todos.@each.isComplete'),
-	createdAt: DS.attr('date', {defaultValue: new Date() }),
-	todos: DS.hasMany('todo', {async: false}),
-	todosCount: Ember.computed('todos', function(){
-		return this.get('todos').get('length');
-	}).property('todos.@each'),
-	todosCountPending: Ember.computed('todos', function(){
-		return this.get('todos').filterBy('isComplete', false).get('length');
-	}).property('todos.@each.isComplete')
+	createdAt: DS.attr('date', { defaultValue: new Date() }),
+	todos: DS.hasMany('todo', { async: false }),
+	isComplete: function(){        
+	    return this.get('todos').filterBy('isComplete', false).get('length') === 0;   
+	}.property('todos.@each.isComplete'),
+	todosCount: function() {        
+		return this.get('todos').get('length');    
+	}.property('todos.@each'),
+	todosCountPending:   function() {        
+	return this.get('todos').filterBy('isComplete', false).get('length');    
+	}.property('todos.@each.isComplete')
 });
